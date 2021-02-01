c4a673b7cfbf7bee5f15256519181f98360f4189
barnez299 <barnez29@gmail.com>
2021-02-01 17:21:49 +0200

    tags model included and working

--- blogapp/models.py
+++ blogapp/models.py
@@ -4,6 +4,12 @@ from app import db
 def slugify(s):
     return re.sub('[^\w]+', '-', s).lower()
 
+
+post_tags = db.Table('post_tags',
+ db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+ db.Column('post_id', db.Integer, db.ForeignKey('post.id'))
+)
+
 class Post(db.Model):
     __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key=True)
@@ -13,14 +19,30 @@ class Post(db.Model):
     created_timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
     modified_timestamp = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
 
-def __init__(self, *args, **kwargs):
-    super(Post, self).__init__(*args, **kwargs) # Call parent constructor
-    self.generate_slug()
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('post', lazy='dynamic'))
 
-def generate_slug(self):
-    self.slug = ''
-    if self.title:
-        self.slug = slugify(self.title)
+    def __init__(self, *args, **kwargs):
+        super(Post, self).__init__(*args, **kwargs) # Call parent constructor
+        self.generate_slug()
 
-def __repr__(self):
-    return '<Post: %s>' % self.title
+    def generate_slug(self):
+        self.slug = ''
+        if self.title:
+            self.slug = slugify(self.title)
+
+    def __repr__(self):
+        return '<Post: %s>' % self.title
+
+
+class Tag(db.Model):
+    __tablename__ = 'tag'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    slug = db.Column(db.String(64), unique=True)
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return '<Tag %s>' % self.name
